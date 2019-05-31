e5a299296a9c3c42c30e66418256b2cc6dbf4d36
CarlW <carl@oskarroger.net>
2019-05-31 11:11:38 +0800

    Fleshing out the a model and building up the endpoint.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./models/laptops");
+
+const app = express();
+
+app.use(bodyParser.json());
