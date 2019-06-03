9e76a08f5b65c6503b47bb88ed4aca25f021d2b3
CarlW <carl@oskarroger.net>
2019-06-03 09:36:41 +0800

    work done on the laptop model, structuring the data.

--- models/laptops.js
+++ models/laptops.js
@@ -1,3 +1,52 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const laptopSchema = new Schema({
+  manufacturer: {
+    type: String,
+    required: true
+  },
+  model: {
+    type: String,
+    required: true
+  },
+  MSRPrelease: {
+    type: Number,
+    required: true
+  },
+  releaseDate: {
+    type: Date,
+    required: true
+  },
+  displaySize: {
+    type: Number,
+    required: true
+  },
+  displayBacklightTechnology: {
+    type: String,
+    required: true
+  },
+  displayPanelType: {
+    type: String,
+    required: true
+  },
+  maxDisplayResolution: {
+    type: String,
+    requied: true
+  },
+  processorManufacturer: {
+    type: String,
+    required: true
+  },
+  numberOfprocessorCores: {
+    type: Number,
+    required: true
+  },
+  processorCoreFrequency: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Event", eventSchema);
