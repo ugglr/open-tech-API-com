661b77fcd4446d22fb0d63d35928d59e18255851
CarlW <carl@oskarroger.net>
2019-06-03 11:17:35 +0800

    Starting with the basic building blocks, Chips, to add them to prod. mod

--- models/gpu.js
+++ models/gpu.js
@@ -2,6 +2,39 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const gpuSchema = new Schema({});
+const gpuSchema = new Schema({
+  gpuType: {
+    type: String,
+    required: true
+    //Desktop GPU
+    //Mobile GPU
+    //Workstation GPU
+    //Mobile Workstation GPU
+    //Grid GPU
+    //Console GPU
+  },
+  manufacturer: {
+    type: String,
+    required: true
+  },
+  modelName: {
+    //The marketing name for the processor, assigned by manufacturer.
+    type: String,
+    required: true
+  },
+  codeName: {
+    /* The internal engineering codename for the processor(typically
+        designated by an NVXY name and later GXY where X is the 
+        series number and Y is the schedule of the project for that
+        generation). */
+    type: String,
+    required: false
+  },
+  fabricationProcess: {
+    /*  Fabrication process. Average feature size of components of the processor. */
+    type: Number,
+    required: true
+  }
+});
 
 module.exports = mongoose.model("GPU", gpuSchema);
