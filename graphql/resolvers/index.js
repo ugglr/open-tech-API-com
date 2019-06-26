c6d16a287af3b1add00487f77d636cfd99960238
CarlW <carl@oskarroger.net>
2019-06-26 16:55:10 +0800

    working on the CPU resolvers

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,3 +2,34 @@ const bcrypt = require("bcryptjs");
 
 const CPU = require("../../models/cpu");
 const GPU = require("../../models/gpu");
+
+const cpus = async cpuIds => {
+  try {
+    const cpus = await CPU.find({ _id: { $in: cpuIds } });
+    cpus.map(cpu => {
+      return {
+        ...cpu._doc,
+        _id: cpu.id
+      };
+    });
+    return cpus;
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  cpus: async () => {
+    try {
+      const cpus = await CPU.find();
+      return cpus.map(cpu => {
+        return {
+          ...cpu._id,
+          _id: cpu.id
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+};
